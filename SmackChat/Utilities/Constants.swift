a1f86450da710c689b2f14a8a9ebf7398967dd04
burn25 <burnham_jeremy@hotmail.com>
2018-05-03 21:56:59 -0400

    login view controller

---
+++ SmackChat/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SmackChat
+//
+//  Created by Jeremy Burnham on 5/3/18.
+//  Copyright © 2018 Jeremy Burnham. All rights reserved.
+//
+
+import Foundation
+
+//Seques
+let TO_LOGIN = "toLogin"
