3a3ae870d3b29f950fa53ad24811c41f9d16c10c
burn25 <burnham_jeremy@hotmail.com>
2018-08-16 21:36:04 -0400

    avitart picker

--- SmackChat/View/AvatarCell.swift
+++ SmackChat/View/AvatarCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum AvatarType {
+    case dark
+    case light
+}
+
 class AvatarCell: UICollectionViewCell {
     
     
@@ -24,6 +29,15 @@ class AvatarCell: UICollectionViewCell {
         self.clipsToBounds = true
     }
     
-    
+    func configureCell(index: Int, type: AvatarType) {
+        if type == AvatarType.dark {
+            avatarImg.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        } else {
+            avatarImg.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+        }
+        
+    }
     
 }
